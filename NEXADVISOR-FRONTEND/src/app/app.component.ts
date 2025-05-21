90c3b4d6db5312887480f52f0f62748d2a11f203
César Martinez <122939628+CesarMR4@users.noreply.github.com>
2025-05-21 12:25:28 -0500

    Instalando angular material - carpetas registro

--- NEXADVISOR-FRONTEND/src/app/app.component.ts
+++ NEXADVISOR-FRONTEND/src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
