79829ebe49c2bff64748d15f7ab3d015911e3f48
Mircco <mircco_hf@hotmail.com>
2025-06-12 19:54:58 -0500

    puntuacion parte 2

---
+++ NEXADVISOR-FRONTEND/src/app/components/ver-puntuacion/ver-puntuacion.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PuntuacionService } from '../../services/puntuacion.service';
+import { Puntuacion } from '../../models/Puntuacion';
+
+@Component({
+  selector: 'app-ver-puntuacion',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ver-puntuacion.component.html',
+  styleUrls: ['./ver-puntuacion.component.css']
+})
+export class VerPuntuacionComponent implements OnInit {
+
+  @Input() idAsesor: number = 0;
+  promedio: number | null = null;
+  cantidad: number = 0;
+
+  constructor(private puntuacionService: PuntuacionService) {}
+
+  ngOnInit(): void {
+    if (this.idAsesor > 0) {
+      this.puntuacionService.listarPorAsesor(this.idAsesor).subscribe({
+        next: (puntuaciones: Puntuacion[]) => {
+          if (puntuaciones.length > 0) {
+            const suma = puntuaciones.reduce((acc, p) => acc + p.puntaje, 0);
+            this.promedio = suma / puntuaciones.length;
+            this.cantidad = puntuaciones.length;
+          } else {
+            this.promedio = null;
+            this.cantidad = 0;
+          }
+        },
+        error: () => {
+          this.promedio = null;
+        }
+      });
+    }
+  }
+}
