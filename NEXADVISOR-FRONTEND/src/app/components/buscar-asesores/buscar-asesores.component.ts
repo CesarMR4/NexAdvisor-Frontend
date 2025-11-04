a0d5cf03acf6f210da0b6e997a10703f96162a00
Mircco <mircco_hf@hotmail.com>
2025-11-04 12:37:06 -0500

    Combo Box - Búsquedas

--- NEXADVISOR-FRONTEND/src/app/components/buscar-asesores/buscar-asesores.component.ts
+++ NEXADVISOR-FRONTEND/src/app/components/buscar-asesores/buscar-asesores.component.ts
@@ -25,6 +25,72 @@ export class BuscarAsesoresComponent {
   cargando: boolean = false;
   errorMensaje: string = '';
 
+    carreras: string[] = [
+    'Administración',
+    'Administración y Ciencia de Datos para Negocios',
+    'Administración y Finanzas',
+    'Administración y Marketing',
+    'Administración y Negocios Internacionales',
+    'Administración y Recursos Humanos',
+    'Arquitectura',
+    'Artes Escénicas',
+    'Artes Contemporáneas',
+    'Biología',
+    'Ciencias de la Actividad Física y el Deporte',
+    'Ciencias de la Computación',
+    'Ciencias Políticas',
+    'Comunicación Audiovisual y Medios Interactivos',
+    'Comunicación y Marketing',
+    'Comunicación e Imagen Empresarial',
+    'Comunicación y Periodismo',
+    'Comunicación y Publicidad',
+    'Contabilidad y Administración',
+    'Contabilidad y Finanzas',
+    'Derecho',
+    'Diseño Industrial',
+    'Diseño Profesional de Interiores',
+    'Diseño Profesional Gráfico',
+    'Diseño y Gestión en Moda',
+    'Economía Gerencial',
+    'Economía y Ciencias de Datos',
+    'Economía y Finanzas',
+    'Economía y Negocios Internacionales',
+    'Educación y Gestión del Aprendizaje',
+    'Enfermería',
+    'Farmacia y Bioquímica',
+    'Gastronomía y Gestión Culinaria',
+    'Hotelería y Administración',
+    'Ingeniería Civil',
+    'Ingeniería Ambiental',
+    'Ingeniería Biomédica',
+    'Ingeniería de Ciberseguridad',
+    'Ingeniería de Gestión Empresarial',
+    'Ingeniería de Gestión Minera',
+    'Ingeniería de Sistemas de Información',
+    'Ingeniería de Software',
+    'Ingeniería Electrónica',
+    'Ingeniería Industrial',
+    'Ingeniería Mecatrónica',
+    'Medicina',
+    'Medicina Veterinaria',
+    'Música',
+    'Nutrición y Dietética',
+    'Odontología',
+    'Psicología',
+    'Relaciones Internacionales',
+    'Terapia Física',
+    'Turismo y Administración'
+  ];
+
+  sectores: string[] = [
+    'Finanzas',
+    'Recursos Humanos',
+    'Tecnología',
+    'Marketing',
+    'Legal',
+    'Consultoría'
+  ];
+
   constructor(private asesorService: AsesorService, private router: Router) {}
 
   buscar() {
